b04fdedc2bf7cc5a7a939c882f695cc844c472c5
tpapp <tkpapp@gmail.com>
2011-10-31 13:57:44 +0100

    Updated X11 examples, they should run now.

--- examples/example-x11.lisp
+++ examples/example-x11.lisp
@@ -2,8 +2,8 @@
 (require :cl-2d)
 (in-package :cl-2d)
 (require :cl-cairo2)
-(require :cl-cairo2-x11)
-(use-package '(:cl-cairo2 :cl-2d :cl-numlib :cl-colors :bind))
+(require :cl-cairo2-xlib)
+(use-package '(:cl-cairo2 :cl-2d :cl-num-utils :cl-colors :bind))
 
 ;;; this is how you create an X11 frame.  If you supply a
 ;;; background-color to as-frame, each plot will clear the frame with
@@ -13,19 +13,19 @@
 			       :background-color +white+))
 
 
-(plot-function *frame* #'exp (interval-of 0 2) :x-title "x" :y-title "exp(x)")
+(plot-function *frame* #'exp (interval 0 2) :x-title "x" :y-title "exp(x)")
 
 ;;; split the frame, and you can draw on the subframes independently.
 ;;; I do this a lot.
 
-(bind ((#2A((f1 f2) (f3 f4)) (split-frame *frame* (percent 50) (percent 50))))
+(bind ((#2A((f1 f2) (f3 f4)) (split-frame *frame* (relative 1/2) (relative 1/2))))
   (defparameter *f1* f1)
   (defparameter *f2* f2)
   (defparameter *f3* f3)
   (defparameter *f4* f4))
 
 (clear *frame*)
-(plot-function *f1* #'sin (interval-of 0 2) :x-title "x" :y-title "sin(x)")
-(plot-function *f2* #'cos (interval-of 0 2) :x-title "x" :y-title "cos(x)")
-(plot-function *f3* #'tan (interval-of 0 2) :x-title "x" :y-title "tan(x)")
-(plot-function *f4* #'/ (interval-of 0 2) :x-title "x" :y-title "1/x")
+(plot-function *f1* #'sin (interval 0 2) :x-title "x" :y-title "sin(x)")
+(plot-function *f2* #'cos (interval 0 2) :x-title "x" :y-title "cos(x)")
+(plot-function *f3* #'tan (interval 0 2) :x-title "x" :y-title "tan(x)")
+(plot-function *f4* #'/ (interval 0 2) :x-title "x" :y-title "1/x")
